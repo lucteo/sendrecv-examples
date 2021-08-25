3f50538328cc68f61a6d2cd0a4f9d67ab08bb682
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2021-08-25 23:04:59 +0300

    Better use of libunifex primitives for continuation examples.

--- basic/continuation_1.cpp
+++ basic/continuation_1.cpp
@@ -12,35 +12,35 @@ struct person {
     std::string name_;
     static_thread_pool pool_{4};
 
-    explicit person(const char* name, bool busy)
+    explicit person(const char* name)
         : name_(name) {}
 
-    void sms(const char* from, const char* message, std::function<void()> callback) {
+    auto sms(const char* from, const char* message) {
         printf("%s is being texted by %s (message: %s)\n", name_.c_str(), from, message);
         printf("    continuing current work...\n");
 
         // Continue working, and then call back
-        auto sched = pool_.get_scheduler();
-        spawn(sched, [=] {
+        auto f = [=] {
             // Finish current work first
             std::this_thread::sleep_for(100ms);
 
             printf("%s is now ready to respond to %s\n", name_.c_str(), from);
-            // Now call back
-            spawn(sched, callback);
-        });
+        };
+        auto sched = pool_.get_scheduler();
+        return schedule(sched) | then(std::move(f));
     }
 };
 
 int main() {
-    person p1("Alice", true);
+    person p1("Alice");
 
     // Try contacting
     auto callback = []() {
         printf("\nCalling back\n");
         std::this_thread::sleep_for(10ms);
     };
-    p1.sms("Bob", "Hey, are you free?", callback);
+    auto s = p1.sms("Bob", "Hey, are you free?") | then(callback);
+    start_detached(std::move(s));
 
     // Do something else while waiting for the message
     {
--- basic/continuation_2.cpp
+++ basic/continuation_2.cpp
@@ -1,4 +1,5 @@
 #include "common.hpp"
+#include <unifex/typed_via.hpp>
 
 #include <string>
 #include <string_view>
@@ -9,48 +10,36 @@ void spawn(Sched&& sched, F&& f) {
     start_detached(std::move(snd));
 }
 
-void print_message_task(const char* msg) {
-
-    printf(" %s", msg);
-
-    std::this_thread::sleep_for(100ms);
-}
-
 struct HttpResponse {
     int status_code_;
     std::string status_line_{};
     std::string body_{};
 };
 
-using response_callback = std::function<void(HttpResponse)>;
+struct http_client {
+    static_thread_pool pool_{4};
 
-template <typename Sched>
-void invoke_callback(Sched&& sched, response_callback cb, HttpResponse resp) {
-    spawn(forward<Sched>(sched), [cb = std::move(cb), resp = std::move(resp)]() mutable {
-        std::move(cb)(std::move(resp));
-    });
-}
+    auto start_async(const char* url) {
+        return schedule(pool_.get_scheduler()) |
+               then([s = std::string{url}] { return process_http_request_impl(s); });
+    }
 
-template <typename Sched>
-void make_http_request(std::string url, Sched&& sched, response_callback cb) {
-    spawn(sched, [=] {
+    static HttpResponse process_http_request_impl(const std::string& url) {
         if (url.find("mutex") != std::string::npos) {
             // Minimal processing
             std::this_thread::sleep_for(10ms);
             // Respond with failure
-            auto resp = HttpResponse{404, "Not found", "Mutexes are not found in user code."};
-            invoke_callback(std::forward<Sched>(sched), std::move(cb), std::move(resp));
+            return HttpResponse{404, "Not found", "Mutexes are not found in user code."};
         } else {
             // Do some processing
             std::this_thread::sleep_for(100ms);
             // Respond with success
-            auto resp = HttpResponse{200, "OK", "There is a better way do do concurrency."};
-            invoke_callback(std::forward<Sched>(sched), std::move(cb), std::move(resp));
+            return HttpResponse{200, "OK", "There is a better way do do concurrency."};
         }
-    });
-}
+    }
+};
 
-void my_callback(HttpResponse&& resp) {
+void print_response(HttpResponse&& resp) {
     printf("Response received: %d %s => %s\n", resp.status_code_, resp.status_line_.c_str(),
             resp.body_.c_str());
 }
@@ -58,7 +47,7 @@ void my_callback(HttpResponse&& resp) {
 void do_something_else() {
     for (int i = 0; i < 10; i++) {
         printf(".");
-        std::this_thread::sleep_for(100us);
+        std::this_thread::sleep_for(10ms);
     }
     printf("\n");
 }
@@ -68,14 +57,12 @@ int main() {
         static_thread_pool my_pool{4};
         auto sched = my_pool.get_scheduler();
 
-        // Make a first request
-        auto f1 = [=] {
-            const char* uri = "google.com?q=mutex";
-            printf("Making request: %s\n", uri);
+        http_client cln;
 
-            make_http_request(uri, sched, my_callback);
-        };
-        spawn(sched, f1);
+        const char* uri = "google.com?q=mutex";
+        printf("Starting request: %s\n", uri);
+        auto s = cln.start_async(uri) | typed_via(sched) | then(print_response);
+        start_detached(std::move(s));
         spawn(sched, do_something_else);
     }
 
@@ -85,13 +72,12 @@ int main() {
         static_thread_pool my_pool{4};
         auto sched = my_pool.get_scheduler();
 
-        auto f2 = [=] {
-            const char* uri = "google.com?q=concurrency";
-            printf("Making request: %s\n", uri);
+        http_client cln;
 
-            make_http_request(uri, sched, my_callback);
-        };
-        spawn(sched, f2);
+        const char* uri = "google.com?q=concurrency";
+        printf("Starting request: %s\n", uri);
+        auto s = cln.start_async(uri) | typed_via(sched) | then(print_response);
+        start_detached(std::move(s));
         spawn(sched, do_something_else);
     }
 
